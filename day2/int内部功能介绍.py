9c20366b6d1c432249b3a44160f155b4f3742279
LI-junting <373738344@qq.com>
2017-10-17 21:17:50 +0800

    今天学习对象和类

---
+++ day2/int内部功能介绍.py
@@ -0,0 +1,13 @@
+age=18
+print(type(age))
+
+import time
+a=time.time()
+print(type(a))
+
+a=8
+b=a.bit_length()  #十进制8 最少需要多少位2进制数来表示
+print(b)
+
+result=a.__eq__(10)  #判断a是否等于10
+print(result)
--- day6/字符学习.py
+++
@@ -1,9 +0,0 @@
-print("你好！")
-print("你好!")
-print("你不还")
-print("hello world")
-
-a=1
-b=2
-c=a+b
-print(c)
